680c564348a62a4ab5410ef00ba01f83711edea5
Michel TANGA <41395250+mtanga@users.noreply.github.com>
2023-10-19 17:32:33 +0100

    Update vite.config.js

--- web/frontend/vite.config.js
+++ web/frontend/vite.config.js
@@ -47,7 +47,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['xlsx'],
+      external: ['xlsx', '@shopify/polaris-viz'],
 /*       output: {
         globals: {
           vue: 'Vue'
